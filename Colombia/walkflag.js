f51df6f61e7045a1379509a52aa5839b8c126262
jenavarro40 <134017786+jenavarro40@users.noreply.github.com>
2023-08-03 22:50:44 -0700

    Add files via upload

--- Colombia/walkflag.js
+++ Colombia/walkflag.js
@@ -6,41 +6,61 @@ var flag=0;
 
 function walk()
 {
+    
+    
     var walk = document.getElementById("flag");
-    topPosition=walk.offsetTop;
-    leftPosition=walk.offsetLeft;
+    if (window.innerWidth> 700)
+    {
+        topPosition=walk.offsetTop;
+        leftPosition=walk.offsetLeft;
+    }
+    else leftPosition=walk.offsetLeft;
     setInterval('leftmovdown()', 200);
+    
 
 }
 function leftmovdown()
+
 {
-
-    if(leftPosition<1200)
+    if (window.innerWidth> 700)
     {
-        if(topPosition<480 && flag===0)
+        if(leftPosition<1200)
         {
-            topPosition=topPosition+20;
-            leftPosition=leftPosition+20;
-            document.getElementById("flag").style.top=topPosition+"px";
-            document.getElementById("flag").style.left=leftPosition+"px";
-            console.log("top: "+topPosition);
-            console.log("left: "+leftPosition);
+            if(topPosition<480 && flag===0)
+            {
+                topPosition=topPosition+20;
+                leftPosition=leftPosition+20;
+                document.getElementById("flag").style.top=topPosition+"px";
+                document.getElementById("flag").style.left=leftPosition+"px";
+
+            }
+            else flag=1;
+            if(topPosition>80 && flag===1)
+            {
+                topPosition=topPosition-20;
+                leftPosition=leftPosition+20;
+                document.getElementById("flag").style.top=topPosition+"px";
+                document.getElementById("flag").style.left=leftPosition+"px";
+
+            }
+            else flag=0;
         }
-        else flag=1;
-        if(topPosition>80 && flag===1)
-        {
-            topPosition=topPosition-20;
-            leftPosition=leftPosition+20;
-            document.getElementById("flag").style.top=topPosition+"px";
-            document.getElementById("flag").style.left=leftPosition+"px";
-            console.log("top: "+topPosition);
-            console.log("left: "+leftPosition);
-        }
-        else flag=0;
+        else document.getElementById("flag").style.display="none";
     }
-    else document.getElementById("flag").style.display="none";
-    
-     
+    else
+    {
+        if(leftPosition<500)
+        {
 
+                leftPosition=leftPosition+30;
 
+                document.getElementById("flag").style.left=leftPosition+"px";
+
+            
+        }
+        else document.getElementById("flag").style.display="none";
+        
+        
+    }  
 }
+
